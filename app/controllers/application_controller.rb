15dd427885bb49488a122f362217ea6a49c59cf5
Lewis Jones <lewismjones@gmail.com>
2017-03-30 23:53:30 -0400

    css formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  http_basic_authenticate_with name: "admin", password: "wyncode", only: [:destroy]
 
-  
+
 end
