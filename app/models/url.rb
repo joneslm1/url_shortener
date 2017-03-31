044922a82fd3b951b582c1ae6da7278202b1ad67
Lewis Jones <lewismjones@gmail.com>
2017-03-31 00:49:19 -0400

    working

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,3 @@
 class Url < ApplicationRecord
+  validate :url_check
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "url#index"
-  get "/index" => "url#index"
+  get "/index" => "url#index", :as => :index_url
 
   get "/new" => "url#new", :as => :new_url
   post "/new" => "url#create"
